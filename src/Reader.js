de93af5edf52f1671bcdeb8e0bc14fdecb104c08
btnguyen <btnguyen@iastate.edu>
2021-07-31 18:07:12 -0500

    First Build

--- src/Reader.js
+++ src/Reader.js
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { book } from './Book_1.js';
 import moment from 'moment';
 
 const words = book.content.split(' ');
 
-function getNextWord() {
-  return moment().toString();;
-}
-
 function Reader() {
-  return (
-    <>
-      <p>
-        { book.title }
-      </p>
-      <p>
-        { getNextWord() }
-      </p>
-    </>
-  );
+  const [time, setTime] = useState(moment());
+  const [initialTime, setInitialTime] = useState(moment());
+  let content = (
+<>
+          <p>
+            { words[Math.floor((time - initialTime) / 150)] }
+          </p>
+          <p>
+            { words[Math.floor((time - initialTime) / 150) + 1] }
+          </p>
+          <p>
+            { words[Math.floor((time - initialTime) / 150) + 2] }
+          </p>
+</>
+      );
+  useEffect(() => {
+      const interval = setInterval(() => setTime(moment()), 5);
+  }, []);
+
+  return content;
 }
 
 export default Reader;
+
