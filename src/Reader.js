53666d3eca9633a8e387b83bd8b14f4e716ef8ec
btnguyen01 <nguyen.ben@principal.com>
2021-08-03 13:30:02 -0500

    added next chapter button

--- src/Reader.js
+++ src/Reader.js
@@ -2,21 +2,23 @@ import React, { useState, useEffect } from 'react';
 import { book } from './Book.js';
 import moment from 'moment';
 
-const chapter = 97;
-const speed = 180;
-
-const words = book.chapter[chapter - 1].content.split(/\s/);
+const chapter = 52;
+const speed = 200;
 
 function Reader() {
+  const [currentChapter, setCurrentChapter] = useState(chapter);
+  let words = book.chapter[currentChapter - 1].content.split(/\s/);
   const [time, setTime] = useState(moment());
   const [initialTime, setInitialTime] = useState(moment());
   let content = (
     <>
       <h5>
-        { book.chapter[chapter - 1].title }
+        { book.chapter[currentChapter - 1].title }
       </h5>
       <p>
-        { words[Math.floor((time - initialTime) / speed)] }
+        { words.length > Math.floor((time - initialTime) / speed)
+        ? words[Math.floor((time - initialTime) / speed)] 
+        : <button onClick={() => {setCurrentChapter(currentChapter + 1);setInitialTime(moment())}}>Next Chapter</button>}
       </p>
     </>
   );
