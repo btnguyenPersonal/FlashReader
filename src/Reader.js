6493fc18c590802a3ddb3356d433dc6486cefca1
btnguyen <btnguyen@iastate.edu>
2021-08-29 10:15:20 -0500

    small formatting changes

--- src/Reader.js
+++ src/Reader.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { book } from './Book.js';
 import moment from 'moment';
+import './App.css';
 
 const chapter = 52;
-const speed = 300;
+const speed = 200;
 
 function Reader() {
   const [currentChapter, setCurrentChapter] = useState(chapter);
@@ -11,18 +12,22 @@ function Reader() {
   const [time, setTime] = useState(moment());
   const [initialTime, setInitialTime] = useState(moment());
   let content = (
-    <>
-      <h5>
-        { book.chapter[currentChapter - 1].title }
-      </h5>
-      <p>
-        { words.length > Math.floor((time - initialTime) / speed)
-        ? words[Math.floor((time - initialTime) / speed)] 
-        : <button onClick={() => {setCurrentChapter(currentChapter + 1);setInitialTime(moment())}}>Next Chapter</button>}
-      </p>
-      <button onClick={() => { setCurrentChapter(currentChapter - 1); setInitialTime(moment()) }}>Chapter {currentChapter - 2}</button>
-      <button onClick={() => { setCurrentChapter(currentChapter + 1); setInitialTime(moment()) }}>Chapter {currentChapter}</button>
-    </>
+      <div className="columns">
+        <button className="button" onClick={() => { setCurrentChapter(currentChapter - 1); setInitialTime(moment()) }}>{'<'}</button>
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            { book.chapter[currentChapter - 1].title }
+          </div>
+          <div class="panel-body">
+            <h4>
+              { words.length > Math.floor((time - initialTime) / speed)
+              ? words[Math.floor((time - initialTime) / speed)] 
+              : <div>End of Chapter</div>}
+            </h4>
+          </div>
+        </div>
+        <button className="button" onClick={() => { setCurrentChapter(currentChapter + 1); setInitialTime(moment()) }}>{'>'}</button>
+      </div>
   );
   useEffect(() => {
       const interval = setInterval(() => setTime(moment()), 5);
