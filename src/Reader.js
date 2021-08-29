194c8a219c98391d1fa1f5505deecb0cd0cb5bf3
btnguyen <btnguyen@iastate.edu>
2021-08-29 11:11:05 -0500

    added media controls still need to reset index after new chapter

--- src/Reader.js
+++ src/Reader.js
@@ -3,34 +3,48 @@ import { book } from './Book.js';
 import moment from 'moment';
 import './App.css';
 
-const chapter = 52;
-const speed = 200;
+let chapter = 1;
+let speed = 200;
+let index = 0;
+let counter = 0;
+
+function incrementIndex() {
+  index++;
+  return true;
+}
 
 function Reader() {
   const [currentChapter, setCurrentChapter] = useState(chapter);
   let words = book.chapter[currentChapter - 1].content.split(/\s/);
   const [time, setTime] = useState(moment());
-  const [initialTime, setInitialTime] = useState(moment());
+  const [isPaused, setIsPaused] = useState(false);
+  const [lastTime, setLastTime] = useState(moment());
   let content = (
       <div className="columns">
-        <button className="button" onClick={() => { setCurrentChapter(currentChapter - 1); setInitialTime(moment()) }}>{'<'}</button>
+        <button className="button" onClick={() => { setCurrentChapter(currentChapter - 1); setLastTime(moment()) }}>{'<'}</button>
         <div class="panel panel-default">
           <div class="panel-heading">
             { book.chapter[currentChapter - 1].title }
           </div>
           <div class="panel-body">
-            <h4>
-              { words.length > Math.floor((time - initialTime) / speed)
-              ? words[Math.floor((time - initialTime) / speed)] 
-              : <div>End of Chapter</div>}
-            </h4>
+            <h2>
+              { 
+                1 < Math.floor((time - lastTime) / speed) && !isPaused
+                ? incrementIndex() && words[index] && setLastTime(moment())
+                : words[index]
+              }
+            </h2>
+            <button className="mediaButton" onClick={() => { speed *= 1.1; counter-- }}>slower</button>
+            <button className="mediaButton" onClick={() => { isPaused ? setIsPaused(false) : setIsPaused(true) }}>{ isPaused ? '>' : '||' }</button>
+            <button className="mediaButton" onClick={() => { speed /= 1.1; counter++ }}>faster</button>
+            <div className="speedIndicator">{counter}</div>
           </div>
         </div>
-        <button className="button" onClick={() => { setCurrentChapter(currentChapter + 1); setInitialTime(moment()) }}>{'>'}</button>
+        <button className="button" onClick={() => { setCurrentChapter(currentChapter + 1); setLastTime(moment()) }}>{'>'}</button>
       </div>
   );
   useEffect(() => {
-      const interval = setInterval(() => setTime(moment()), 5);
+      setInterval(() => setTime(moment()), 1);
   }, []);
 
   return content;
