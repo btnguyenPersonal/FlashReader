d7a43ff151afca40e8e8525785d2e5015a914d00
btnguyenPersonal <btnguyenPersonal@github.com>
2021-09-22 09:22:27 -0500

    added better keybinds

--- src/Reader.js
+++ src/Reader.js
@@ -21,20 +21,17 @@ function incrementIndex() {
 
 function Reader() {
   mousetrap.bind('k', function() {isPaused ? setIsPaused(false) : setIsPaused(true)});
-  mousetrap.bind('K', function() {isPaused ? setIsPaused(false) : setIsPaused(true)});
   mousetrap.bind('space', function() {isPaused ? setIsPaused(false) : setIsPaused(true); return false});
   mousetrap.bind('l', function() {skipAhead()});
-  mousetrap.bind('L', function() {skipAhead()});
   mousetrap.bind('j', function() {skipBack()});
-  mousetrap.bind('L', function() {skipBack()});
   mousetrap.bind('-', function() {slower()});
-  mousetrap.bind('_', function() {slower()});
   mousetrap.bind('=', function() {faster()});
-  mousetrap.bind('+', function() {faster()});
-  mousetrap.bind('{', function() {setCurrentChapter(currentChapter - 1)});
-  mousetrap.bind('[', function() {setCurrentChapter(currentChapter - 1)});
-  mousetrap.bind('}', function() {setCurrentChapter(currentChapter + 1)});
-  mousetrap.bind(']', function() {setCurrentChapter(currentChapter + 1)});
+  mousetrap.bind('s', function() {slower()});
+  mousetrap.bind('f', function() {faster()});
+  mousetrap.bind('up', function() {skipAhead()});
+  mousetrap.bind('down', function() {skipBack()});
+  mousetrap.bind('left', function() {setChapter(currentChapter - 1)});
+  mousetrap.bind('right', function() {setChapter(currentChapter + 1)});
 
   function faster() {
     if (counter < 20) {
