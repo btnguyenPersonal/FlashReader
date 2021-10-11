c6f68a3c9f29c5857cc69bb02040eee15cf9d003
btnguyenPersonal <btnguyenPersonal@github.com>
2021-10-11 09:12:21 -0500

    separating the getWord() logic out into its own function

--- src/Reader.js
+++ src/Reader.js
@@ -20,19 +20,26 @@ function incrementIndex() {
 }
 
 function Reader() {
-  mousetrap.bind('k', function() {isPaused ? setIsPaused(false) : setIsPaused(true)});
-  mousetrap.bind('space', function() {isPaused ? setIsPaused(false) : setIsPaused(true); return false});
+  mousetrap.bind('k', function() {pause()});
+  mousetrap.bind('space', function() {pause(); return false});
   mousetrap.bind('l', function() {skipAhead()});
   mousetrap.bind('j', function() {skipBack()});
   mousetrap.bind('-', function() {slower()});
   mousetrap.bind('=', function() {faster()});
   mousetrap.bind('s', function() {slower()});
   mousetrap.bind('f', function() {faster()});
-  mousetrap.bind('up', function() {skipAhead()});
-  mousetrap.bind('down', function() {skipBack()});
+  mousetrap.bind('up', function() {skipAhead(); return false});
+  mousetrap.bind('down', function() {skipBack(); return false});
   mousetrap.bind('left', function() {setChapter(currentChapter - 1)});
   mousetrap.bind('right', function() {setChapter(currentChapter + 1)});
 
+  function pause() {
+    if (isPaused) {
+      setIsPaused(false);
+    } else {
+      setIsPaused(true);
+    }
+  }
   function faster() {
     if (counter < 20) {
       counter++;
@@ -59,6 +66,17 @@ function Reader() {
     setCurrentChapter(c);
     setLastTime(moment());
   }
+  function getWord(time, lastIndex, isPaused) {
+    if ((1 < Math.floor((time - lastTime) / speed)) && !isPaused) {
+      if ( index < words.length - 2 ) {
+        incrementIndex(); 
+        setLastTime(moment());
+      } else {
+        setChapter(currentChapter + 1);
+      }
+    }
+    return words[index]
+  }
   const [currentChapter, setCurrentChapter] = useState(chapter);
   let words = book.chapter[currentChapter - 1].content.split(/\s/);
   const [time, setTime] = useState(moment());
@@ -73,15 +91,14 @@ function Reader() {
           </div>
           <div class="panel-body">
             <h2>
-              { 
-                1 < Math.floor((time - lastTime) / speed) && !isPaused
-                ? index <= words.length + 1 ? incrementIndex() && words[index] && setLastTime(moment()) : setChapter(currentChapter + 1)
-                : words[index]
-              }
+              { getWord(time, lastTime, isPaused) }
             </h2>
+            <p4>
+              { index }
+            </p4>
             <button className="mediaButton" onClick={() => { skipBack() }}>{'<<'}</button>
             <button className="mediaButton" onClick={() => { slower() }}>slower</button>
-            <button className="mediaButton" onClick={() => { isPaused ? setIsPaused(false) : setIsPaused(true) }}>{ isPaused ? '>' : '||' }</button>
+            <button className="mediaButton" onClick={() => { pause() }}>{ isPaused ? '>' : '||' }</button>
             <button className="mediaButton" onClick={() => { faster() }}>faster</button>
             <button className="mediaButton" onClick={() => { skipAhead() }}>{'>>'}</button>
             <div className="speedIndicator">{counter < 20 ? counter : 'MAX'}</div>
